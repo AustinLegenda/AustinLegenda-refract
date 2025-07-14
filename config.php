87d65e143f4056dfe88573269b23061b86b737dd
AustinLegenda <austin@legenda.co>
2025-07-14 16:27:53 -0400

    commit

--- config.php
+++ config.php
@@ -1,8 +1,8 @@
 <?php
 
-//define('DB_HOST', '157.245.209.9');
+define('DB_HOST', '157.245.209.9');
 define('DB_NAME', 'mdpngsfhzc');
 define('DB_USER', 'mdpngsfhzc');
 define('DB_PASS', 'Qj4B2z3fWt');
-//$conn = new PDO("mysql:host=157.245.209.9;dbname=mdpngsfhzc", "mdpngsfhzc", "Qj4B2z3fWt");
+$conn = new PDO("mysql:host=157.245.209.9;dbname=mdpngsfhzc", "mdpngsfhzc", "Qj4B2z3fWt");
 ?>
