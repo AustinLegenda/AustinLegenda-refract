4d33971de42780c5395beb7829476faa04aaa89b
AustinLegenda <austin@legenda.co>
2025-07-14 20:28:55 -0400

    commit

--- includes/Hooks/LoadData.php
+++ includes/Hooks/LoadData.php
@@ -19,7 +19,7 @@ class LoadData
         $dataRows = $data['data'];
 
         // 2) Connect to DB
-        require_once __DIR__ . '/../config.php';
+        require_once __DIR__ . '/config.php';
         $pdo = new PDO(
             "mysql:host=" . DB_HOST . ";dbname=" . DB_NAME . ";charset=utf8mb4",
             DB_USER,
