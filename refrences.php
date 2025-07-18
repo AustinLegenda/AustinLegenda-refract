94042dccfca9331e0412af19a3cd0fa41f6dd8ce
AustinLegenda <austin@legenda.co>
2025-07-18 15:15:40 -0400

    commit

---
+++ refrences.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+</head>
+
+<body>
+    <h2>NOAA</h2>
+
+    <ul>
+        <li>
+            <a href="https://www.ndbc.noaa.gov/station_page.php?station=41112">Station 41112</a>
+
+        </li>
+        <li>
+            <a href="https://www.ndbc.noaa.gov/station_page.php?station=41117">Station 41117</a>
+        </li>
+        <li>
+            <a href="https://polar.ncep.noaa.gov/nwps/viewer.shtml">Nearshore Wave Prediction System</a>
+        </li>
+        <li>
+            <a href="https://nauticalcharts.noaa.gov/enconline/enconline.html">NOAA ENC Viewer</a>
+        </li>
+    </ul>
+
+    <body>
+    <h2>CDIP</h2>
+
+    <ul>
+        <li>
+            <a href="https://cdip.ucsd.edu/m/products/?stn=132p1">Fernandian Beach, FL. - 132</a>
+
+        </li>
+   
+    </ul>
+</body>
+
+</html>
